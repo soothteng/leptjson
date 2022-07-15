f8e9127f62dd562bd919674714af260840bfbc34
Adam-Teng <656081024@qq.com>
2022-07-15 12:15:41 +0800

    [feat] add number parse

--- src/test.c
+++ src/test.c
@@ -139,6 +139,7 @@ static void test_parse() {
   test_parse_null();
   test_parse_true();
   test_parse_false();
+  test_parse_number();
   test_parse_expect_value();
   test_parse_invalid_value();
   test_parse_root_not_singular();
